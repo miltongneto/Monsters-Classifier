d66a33c959053d190e1011bd2573bfd9f07747d3
miltongneto <mvgn@cin.ufpe.br>
2017-12-03 23:06:22 -0200

    treinamento de MLP e utilizacao do conjunto de teste. Escrita no csv no formato de submissao

--- classifier.py
+++ classifier.py
@@ -2,38 +2,70 @@ import numpy as np
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neural_network import MLPClassifier
 from sklearn.model_selection import cross_val_score
+from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import LabelEncoder
 
 class Classifier(object):
     
-    def __init__(self, train_set):
+    def __init__(self, train_set, test_set):
         self.train_set = train_set
+        self.test_set = test_set
         self.train_set_features = None
         self.train_set_labels = None
+        self.test_set_features = None
+        self.test_set_labels = None
 
     def preProcess(self):
-        self.train_set_labels = self.train_set['type'].copy()
-        colors = self.train_set['color'].copy()
+        self.train_set_features = self.preProcessSet('train')
+        self.test_set_features = self.preProcessSet('test')
+
+    def preProcessSet(self, type):
+        dataset = None
+        dataset_features = None
+        if type == 'train':
+            dataset = self.train_set
+            self.train_set_labels = self.train_set['type'].copy()
+            dataset = dataset.drop('type', axis=1)
+        else:
+            dataset = self.test_set
+
+        colors = dataset['color'].copy()
         colors_encoded = pd.get_dummies(colors)
-        self.train_set_features = self.train_set.drop(['id', 'type', 'color'], axis=1)
-        self.train_set_features['black'] = colors_encoded['black']
-        self.train_set_features['blood'] = colors_encoded['blood']
-        self.train_set_features['blue'] = colors_encoded['blue']
-        self.train_set_features['clear'] = colors_encoded['clear']
-        self.train_set_features['green'] = colors_encoded['green']
-        self.train_set_features['white'] = colors_encoded['white']
+        dataset_features = dataset.drop(['id', 'color'], axis=1)
+        dataset_features['black'] = colors_encoded['black']
+        dataset_features['blood'] = colors_encoded['blood']
+        dataset_features['blue'] = colors_encoded['blue']
+        dataset_features['clear'] = colors_encoded['clear']
+        dataset_features['green'] = colors_encoded['green']
+        dataset_features['white'] = colors_encoded['white']
+
+        return dataset_features
 
     def classification(self):
-        print("Random Forest:")
-        clf = RandomForestClassifier(criterion="entropy", n_jobs=-1)
-        scores = cross_val_score(clf, self.train_set_features, self.train_set_labels, cv=10, scoring="accuracy")
-        print(scores)
+        # print("Random Forest:")
+        # clf = RandomForestClassifier(criterion="entropy", n_jobs=-1)
+        # scores = cross_val_score(clf, self.train_set_features, self.train_set_labels, cv=10, scoring="accuracy")
+        # print(np.mean(scores))
 
-        print("KNN")
-        clf = KNeighborsClassifier(n_neighbors=5)
-        scores = cross_val_score(clf, self.train_set_features, self.train_set_labels, cv=10, scoring="accuracy")
-        print(scores)
+        # print("KNN")
+        # clf = KNeighborsClassifier(n_neighbors=5)
+        # scores = cross_val_score(clf, self.train_set_features, self.train_set_labels, cv=10, scoring="accuracy")
+        # print(np.mean(scores))
+
+        # print("MLP")
+        # clf = MLPClassifier(hidden_layer_sizes=(7,), max_iter=1200)
+        # scores = cross_val_score(clf, self.train_set_features, self.train_set_labels, cv=10, scoring='accuracy')
+        # print(np.mean(scores))
+
+        clf = MLPClassifier(hidden_layer_sizes=(7,), max_iter=1200)
+        clf.fit(self.train_set_features, self.train_set_labels)
+        predict = clf.predict(self.test_set_features)
+
+        return predict
+
 
     def execute(self):
         self.preProcess()
-        self.classification()
+        return self.classification()
--- main.py
+++ main.py
@@ -3,8 +3,12 @@ from classifier import Classifier
 import pandas as pd
 
 train_set = pd.read_csv('train.csv', header=0, index_col=None)
+test_set = pd.read_csv('test.csv', header=0, index_col=None)
 
 # analyzer = Analyzer(train_set)
 # analyzer.process()
-classifier = Classifier(train_set)
-classifier.execute()
+classifier = Classifier(train_set, test_set)
+result = pd.DataFrame(test_set['id'].copy())
+predict = classifier.execute()
+result['type'] = predict
+result.to_csv("result.csv", sep=',', index=False)
